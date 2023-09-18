b83523e5b078c910ff517c7cc9742db976da69d2
RAABIN SINGH  S A <112077212+raabin10@users.noreply.github.com>
2023-09-18 20:00:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function decimalToBinary(num) {
-  //Write you code here
+  // Handle the case when the input is 0
+  if (num === 0) {
+    return '0';
+  }
+
+  let binary = '';
   
+  // Convert the decimal number to binary using repeated division by 2
+  while (num > 0) {
+    const remainder = num % 2;
+    binary = remainder + binary;
+    num = Math.floor(num / 2);
+  }
+  
+  return binary;
 }
 
+// Export the function so it can be used in other scripts
 window.decimalToBinary = decimalToBinary;
